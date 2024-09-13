643aa20502a84c5383c4d10faddd78d99e2476fc
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2024-09-13 17:01:10 -0300

    responsive products option nav, responsive products table

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,23 +13,23 @@
                     <table class="table-auto m-4 w-full  ">
                         <thead>
                           <tr class="">
-                            <th class="border border-slate-600">ID</th>
+                            <th class="border border-slate-600 hidden lg:block">ID</th>
                             <th class="border border-slate-600">Name</th>
                             <th class="border border-slate-600">Price</th>
-                            <th class="border border-slate-600">Created</th>
+                            <th class="hidden lg:block border border-slate-600">Created at</th>
                             <th class="border border-slate-600">Actions</th>
                           </tr>
                         </thead>
                         <tbody class="">
                             @foreach ($products as $product)                                
                                 <tr class="border-b border-gray-300">
-                                    <td >{{$product->id}}</td>
+                                    <td  class="hidden lg:block">{{$product->id}}</td>
                                     <td>{{$product->name}}</td>
-                                    <td>{{$product->price}}</td>
-                                    <td>{{$product->created_at}}</td>
-                                    <td>                          
-                                        <a class="bg-blue-500 text-white px-1 rounded" href="{{route('products.show',$product)}}"><i class="fa-solid fa-circle-info"></i></a>                                    
-                                        <a class="bg-gray-800 text-white px-1 rounded" href="{{route('products.edit',$product)}}"><i class="fa-solid fa-pen-to-square"></i></a>                                    
+                                    <td>${{$product->price}}</td>
+                                    <td class="hidden lg:block">{{$product->created_at}}</td>
+                                    <td class="text-center">                          
+                                        <a class="bg-green-500 text-white px-1 rounded" href="{{route('products.show',$product)}}"><i class="fa-solid fa-eye"></i></a>                                    
+                                        <a class="text-gray-600 px-1 rounded" href="{{route('products.edit',$product)}}"><i class="fa-solid fa-pen-to-square"></i></a>                                    
                                     </td>
                                    
                                 </tr>
