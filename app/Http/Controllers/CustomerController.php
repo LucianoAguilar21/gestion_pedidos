08c06ce28d062c38ca37da5614d42c4241dc4761
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2024-09-17 14:27:51 -0300

    Customer policy

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class CustomerController extends Controller
 {
@@ -55,7 +56,11 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        return view('customers.show')->with(['customer'=>$customer]);
+        if(Gate::authorize('view',$customer)){
+            return view('customers.show')->with(['customer'=>$customer]);
+        }else{
+            return route('customers');
+        }
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
 use App\Models\Product;
+use App\Policies\CustomerPolicy;
 use App\Policies\ProductPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -25,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Product::class, ProductPolicy::class);
+        Gate::policy(Customer::class, CustomerPolicy::class);
     }
 }
